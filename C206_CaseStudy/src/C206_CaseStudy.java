eb10a9152ab71b13c1779447961608d7cc0879ee
Kai <20014063@myrp.edu.sg>
2021-08-09 23:06:27 +0800

    MEMBER 5 DEAL Complete

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -5,7 +5,37 @@ public class C206_CaseStudy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<deal> dealList = new ArrayList<deal>();
-		String SearchInput = Helper.readString("Enter keyword: ");
+		deal d1 = new deal(0, "chair", "Von@email", "Franc@email", 20.00, "8/8");
+		deal d2 = new deal(1, "table", "Bobert@email", "john@email", 50.00, "6/6");
+		dealList.add(d1);
+		dealList.add(d2);
+		
+		int option = 0;
+		
+		while(option != 4) {
+
+			message();
+			option = Helper.readInt("Enter your input > ");
+			
+			if(option == 1) {
+				//Add
+				deal dl = inputDeal();
+				addDeal(dealList, dl);
+				
+			}
+			else if(option == 2) {
+				//View
+				C206_CaseStudy.viewAllDeal(dealList);
+			}
+			else if(option == 3) {
+				//Delete
+				int deleteInput = Helper.readInt("Enter ID to delete > ");
+				deleteDeal(dealList, deleteInput);
+			}
+		}
+		System.out.println("Good Bye");
+		
+		
 
 			
 		
@@ -16,9 +46,10 @@ public class C206_CaseStudy {
 		String output = "";
 		
 		for(int i=0; i<dealList.size(); i++) {
+			deal get = dealList.get(i);
 			
-			int dealID = dealList.get(i).getId();
-			output += String.format("", dealList.get(i).toString());
+			output += String.format("%-5d %-15s %-15s %-15s %-10.2f %-10s", get.getId(), get.getName(), get.getBuyerEmail(), get.getSellerEmail(), get.getPrice(), get.getClosedate());
+			output += "\n";
 		}
 		
 		return output;
@@ -26,7 +57,7 @@ public class C206_CaseStudy {
 	
 	public static void viewAllDeal(ArrayList<deal> dealList) {
 		String output = "DEAL LIST\n";
-		output += String.format("%-10s %-15s %-30s %-30s %-10s %-10s", "ID","ITEM NAME","SELLER","BUYER","PRICE","DUE DATE");
+		output += String.format("%-5s %-15s %-15s %-15s %-10s %-10s\n", "ID","ITEM NAME","SELLER","BUYER","PRICE","DUE DATE");
 		output += retrieveAllDeal(dealList);
 		System.out.println(output);
 	}
@@ -40,24 +71,24 @@ public class C206_CaseStudy {
 		double price = Helper.readDouble("Enter transcation price > ");
 		String closeDate = Helper.readString("Enter close date > ");
 		
-		deal dl = new deal(dealID, itemName, sellerEmail, buyerEmail, price, closeDate);
+		deal dl = new deal(dealID, itemName, buyerEmail, sellerEmail, price, closeDate);
 		return dl;
 	}
 	
-	public static void addDeal(ArrayList<deal> dealList, deal dl) {
+	public static Boolean addDeal(ArrayList<deal> dealList, deal dl) {
 		
 		dealList.add(dl);
 		System.out.println("Deal added");
+		return true;
 	}
 	
 	
 	//=========== Option 3 Delete (CRUD - Read) =================================
 	
-	public static int findDeal(ArrayList<deal> dealList) {
+	public static int findDeal(ArrayList<deal> dealList, int deleteInput) {
 		int deleteIndex = 0;
-		String searchKeyword = Helper.readString("Search > ");
 		for(int i=0; i<dealList.size(); i++) {
-			if(dealList.get(i).toString().contains(searchKeyword)) {
+			if(dealList.get(i).getId() == deleteInput) {
 				deleteIndex = i;
 			}
 		}
@@ -65,43 +96,26 @@ public class C206_CaseStudy {
 		return deleteIndex;
 	}
 	
-	public static void deleteDeal(ArrayList<deal> dealList, int index) {
-		dealList.remove(index);
+	public static void deleteDeal(ArrayList<deal> dealList, int deleteInput) {
+
+		dealList.remove(findDeal(dealList, deleteInput));
 		System.out.println("Deal successfully deleted!");
 	}
 	
 	
-	//=========== Option 4 search deal (CRUD - Read) =================================
+
 	
-	public static int searchDeal(ArrayList<deal> dealList) {
-		int findIndex = 0;
-		String searchKeyword = Helper.readString("Search > ");
-		for(int i=0; i<dealList.size(); i++) {
-			if(dealList.get(i).toString().contains(searchKeyword)) {
-				findIndex = i;
-			}
-		}
-		
-		return findIndex;
-	}
 	
-	public static void showfindDeal(ArrayList<deal> dealList, int findIndex) {
-		System.out.println(dealList.get(findIndex));
-	}
+	//====================== Menu Message Output ====================================
 	
-	//=========== Option 4 search deal (CRUD - Read) =================================
-	
-	public static deal updateDeal() {
-		
-		int dealID = Helper.readInt("Enter deal ID > ");
-		String itemName = Helper.readString("Enter item name > ");
-		String sellerEmail = Helper.readString("Enter seller email > ");
-		String buyerEmail = Helper.readString("Enter buyer email > ");
-		double price = Helper.readDouble("Enter transcation price > ");
-		String closeDate = Helper.readString("Enter close date > ");
-		
-		deal updateDL = new deal(dealID, itemName, sellerEmail, buyerEmail, price, closeDate);
-		return updateDL;
+	public static void message() {
+		String message = "-------------------";
+		message += "\n1.Add deal";
+		message += "\n2.View deal";
+		message += "\n3.Delete deal";
+		message += "\n4.Exit";
+		message += "\n-------------------";
+		System.out.println(message);
 	}
 	
 }
--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,13 +1,21 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
+	
+	private ArrayList<deal> dealList;
+	private deal d1, d2;
 
 	@Before
 	public void setUp() throws Exception {
+		d1 = new deal(0, "chair", "Von@email", "Franc@email", 20.00, "8/8");
+		d2 = new deal(1, "table", "Bobert@email", "john@email", 50.00, "6/6");
+		dealList = new ArrayList<deal>();
 	}
 
 	@After
@@ -20,5 +28,61 @@ public class C206_CaseStudyTest {
 		assertTrue("C206_CaseStudy_SampleTest ",true);
 	}
 	
+	@Test
+	public void addDealTest() {
+		// deal list is not null, so that can add a new deal - boundary
+		assertNotNull("Check if there is valid deal arraylist to add to", dealList);
+		//Given an empty list, after adding 1 item, the size of the list is 1 - normal
+		//The item just added is as same as the first item of the list
+		C206_CaseStudy.addDeal(dealList, d1);
+		assertEquals("Check that deal arraylist size is 1", 1, dealList.size());
+		assertSame("Check that deal is added", d1, dealList.get(0));
+		
+		//Add another item. test The size of the list is 2? -normal
+		//The item just added is as same as the second item of the list
+		C206_CaseStudy.addDeal(dealList, d2);
+		assertEquals("Check that deal arraylist size is 2", 2, dealList.size());
+		assertSame("Check that deal is added", d2, dealList.get(1));
+	}
 
+	@Test
+	public void retrieveAllDeals() {
+		// Test if Item list is not null but empty -boundary
+		assertNotNull("Test if there is valid deal arraylist to retrieve item", dealList);
+		
+		//test if the list of deal retrieved from the SourceCentre is empty - boundary
+		String allDeal= C206_CaseStudy.retrieveAllDeal(dealList);
+		String testOutput = "";
+		assertEquals("Check that ViewAllCamcorderlist", testOutput, allDeal);
+		
+		//Given an empty list, after adding 2 items, test if the size of the list is 2 - normal
+		C206_CaseStudy.addDeal(dealList, d1);
+		C206_CaseStudy.addDeal(dealList, d2);
+		assertEquals("Test that Camcorder arraylist size is 2", 2, dealList.size());
+		
+		//test if the expected output string same as the list of deal retrieved from the SourceCentre	
+		allDeal= C206_CaseStudy.retrieveAllDeal(dealList);
+		testOutput = String.format("%-5s %-15s %-15s %-15s %-10s %-10s\n", "0","chair","Franc@email","Von@email","20.00","8/8");
+		testOutput += String.format("%-5s %-15s %-15s %-15s %-10s %-10s\n", "1","table","john@email","Bobert@email","50.00","6/6");
+	
+		assertEquals("Test that ViewAllDeal", testOutput, allDeal);
+		
+	}
+	
+	@Test
+	public void deleteDeals_test() {
+		// Deal  list is not null, so that can add a new deal - boundary
+		assertNotNull("Check if there is valid deal arraylist to add to", dealList);
+		
+		C206_CaseStudy.addDeal(dealList, d1);
+		assertEquals(1, dealList.size());
+		C206_CaseStudy.addDeal(dealList, d2);
+		assertEquals(2, dealList.size());
+		
+		C206_CaseStudy.deleteDeal(dealList, 0);
+		C206_CaseStudy.deleteDeal(dealList, 1);
+		
+	}
+	
+	
 }
